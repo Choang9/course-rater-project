16f1e1e169d7d1a986c4c41c7a7cf37aa8948b61
Choang9 <choang@seattleu.edu>
2021-07-28 16:42:25 -0700

    Fix base url

--- client/src/apis/CourseRater.js
+++ client/src/apis/CourseRater.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://course-rater-project.herokuapp.com/api/courses"
+    baseURL:
+        process.env.NODE_ENV !== "production"
+            ? "http://localhost:3006/api/courses"
+            : "https://course-rater-project.herokuapp.com/api/courses",
+    timeout: 5000,
 })
