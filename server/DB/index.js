58d599a11af5341a7d17366391fe732815169b21
Choang9 <choang@seattleu.edu>
2021-07-28 16:25:23 -0700

    Update pool

--- server/DB/index.js
+++ server/DB/index.js
@@ -1,6 +1,13 @@
-const { Pool } = require('pg')
+const { Pool } = require("pg");
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
+const pool = new Pool({
+  connectionString: isProduction
+    ? process.env.DATABASE_URL // Heroku will supply us with a string called DATABASE_URL for the connectionString,
+    : connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
+});
 
-const pool = new Pool();
 module.exports = {
   query: (text, params) => pool.query(text, params),
-}
+};
