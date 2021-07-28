40edb4ba24a52f5d9c8aa87db752fe6a551e0a1c
Choang9 <choang@seattleu.edu>
2021-07-28 16:18:26 -0700

    Update db.sql

--- server/DB/db.sql
+++ server/DB/db.sql
@@ -1,3 +1,10 @@
+CREATE TABLE courses (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    subject VARCHAR(50) NOT NULL,
+    instructor VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE reviews (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     course_id BIGINT NOT NULL REFERENCES courses(id),
@@ -6,6 +13,4 @@ CREATE TABLE reviews (
     rating INT NOT NULL check(rating >= 1 and rating <= 5)
 );
 
-INSERT INTO reviews (course_id, name, review, rating) VALUES (1, 'Tom', 'Great course!', 5);
-
-"select * from courses left join (select course_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by course_id) reviews on courses.id = reviews.course_id;"
+INSERT INTO courses (name, subject, instructor) VALUES ('Math 1100- Intro to Algebra', 'Mathematics', 'John Doe');
